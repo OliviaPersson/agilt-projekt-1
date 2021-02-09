16c36c280d6c4e167287c214ec2cd37dc569e8f8
FilipStang <72440167+FilipStang@users.noreply.github.com>
2021-02-09 13:09:34 +0100

    Update RPG.py
    startmeny till rpg

---
+++ RPG.py
@@ -0,0 +1,30 @@
+#Implements a start menu for a adventure type game.
+
+def start():
+    count = 0
+    while True:
+        #Prints a nintro message
+        print("Suddenly you awake...")
+        print("You are in a empty dark room...")
+        print("You see two doors: one on the [right] and one on the [left]...")
+        print("Which door do you choose ?")
+
+        input = input("> ")
+        #All possible choices
+        if input  == "right" and count  < 3:
+            #call a room function
+            pass
+        if input == "left" and count < 3:
+            #Call a room function
+            pass
+        if count >=3:
+            #Death function call 
+            pass
+        else:
+            #No valid input from player
+            print("You look to your [left] and [right]...")
+            print("and decide to take a nap.")
+
+        
+        count = count +1 
+
