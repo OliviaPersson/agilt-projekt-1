3f5dc0cef76ac30e184261717606112a9c703b57
Oscar Carlsson <Oscar.carlsson@LIVE.COM>
2021-02-09 13:59:13 +0100

    ännu en tung funktion

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,3 +1,12 @@
 def computer_choice():
     from random import randint
     return randint(1,3)
+
+
+def best_of_10_winner(comp_score, player_score):
+    if player_score > comp_score:
+        print("You win!!")
+    elif player_score == comp_score:
+        print("Tie!!")
+    else:
+        print("You lose!!")
