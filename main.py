a2341b42985981b3f01a4088c43c6dd1917fd496
FilipStang <72440167+FilipStang@users.noreply.github.com>
2021-02-09 11:55:48 +0100

    Update main.py
    main menu

---
+++ main.py
@@ -0,0 +1,43 @@
+def main_menu():
+    while True:
+        
+        #Prints a welcome message.
+        #Prints choices
+        print("******************************************")
+        print("Welcome to group 5's project games ")
+        print("Choose a game !!")
+        print("Type [A] to play an adventure game.")
+        print("Type [G] to play a guess a number game.")
+        print("Type [RPS] to play rock papper scissors.")
+        print("Type [TT] to play tic-tac-toe.")
+        print("Type [E] to exit application.")
+        print("*******************************************")
+
+        if True:
+            #Guess a number function call herer.
+            pass
+        if True:
+            #RPG function call here.
+            pass
+        elif True:
+            #Rock papper scissors function call here.
+            pass
+        elif True:
+            #Tic Tac Toe function call here.
+            pass
+        elif True:
+            #Exits application on [E] command.
+
+            exit(0)
+            pass
+        else:
+            #
+            print("You have not typed in a valid command")
+            print("Please try again.")
+
+
+
+    #test
+    
+    
+    pass
