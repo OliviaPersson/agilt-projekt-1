ce89da9dc613d8f00c1c141c341d6c5fc80d3b38
Oscar Carlsson <Oscar.carlsson@LIVE.COM>
2021-02-09 14:15:02 +0100

    mindra ändring

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,9 +1,11 @@
 def computer_choice():
+    #Generates random choice for computer
     from random import randint
     return randint(1,3)
 
 
-def best_of_10_winner(comp_score, player_score):
+def best_of_winner(comp_score, player_score):
+    #Checks who has the most wins
     if player_score > comp_score:
         print("You win!!")
     elif player_score == comp_score:
