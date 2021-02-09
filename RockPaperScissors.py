e5f68d67ae9d50fb510512839e5f343daca1f599
LindaHolmberg <69193585+LindaHolmberg@users.noreply.github.com>
2021-02-09 14:03:02 +0100

    print winner and score

---
+++ RockPaperScissors.py
@@ -0,0 +1,3 @@
+def print_winner_and_score(winner,player1,player2):
+    #Print who won the round and the current score.
+    print(f'{winner} has won!\nThe score is: \nPlayer 1: {player1}\nPlayer 2: {player2}')
